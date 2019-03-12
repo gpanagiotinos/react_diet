9f912da1aadfde74a2f8633b9a173bcf8786caf9
georgepan <georgepan@expressdigibooks.com>
2019-03-12 14:39:16 +0200

    link login

--- tests/testLogin.js
+++ tests/testLogin.js
@@ -1,6 +1,7 @@
 import chai from 'chai'
 import chaiHttp from 'chai-http'
 import {app} from '../src/api/server.js'
+// import config from ''
 
 chai.use(chaiHttp)
 chai.should()
@@ -12,10 +13,13 @@ describe("Login", () => {
         .post('/user/login')
         .type('json')
         .send({
-          
+          'username': 'george',
+          'password': '123456'
         })
         .end((err, res) => {
-
+          res.should.have.status(200)
+          res.should.have.cookie('sid')
+          done()
         })
     })
   })
